dd24fb48a9a47b17b37d5a58abf34b2ef9d1502d
shraderdm <shraderdm@gmail.com>
2017-01-14 00:34:55 -0500

    Update h2-server.sh

--- h2/h2-server.sh
+++ h2/h2-server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 dir=$(dirname "$0")
-java -cp "$dir/h2-1.3.175.jar:" org.h2.tools.Server \
-	-properties '/opt/h2-conf/' -baseDir '/opt/h2-data/' \
- 	-web -webAllowOthers -webPort 81 \
- 	-tcp -tcpAllowOthers -tcpPort 1521
+java -cp "$dir/h2-1.3.175.jar:" h2*.jar org.h2.tools.Recover
+
+mkdir old
+move qie.h2.db old\qie.h2.db
+move qie.trace.db old\qie.trace.db
+java -cp h2*.jar org.h2.tools.RunScript -url jdbc:h2:./qie -user sa -script qie.h2.sql
+del qie.h2.sql
